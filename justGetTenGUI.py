fd0cb0bda6edeadafa90d2b8bfe5f633d6455575
3KyNoX <t.prost@everdreamsoft.com>
2017-01-21 23:47:20 +0100

    More music, finished timer + new time limit game mode

--- justGetTenGUI.py
+++ justGetTenGUI.py
@@ -1,9 +1,6 @@
 """
 @ToDo Python
-- Laura : Put save file on save folder
-- Documentation of the code (fully done)
-- Timer game modes
-- Some animations at cells startup placement and on cells fusion
+- Missing animations at startup / modification / gravity -> not enough time
 """
 
 # -*- coding: utf-8 -*-
@@ -17,18 +14,21 @@ pygame.init()
 
 def main():
     # Init vars
-    global gameArea, gameBoard, mapSize, surface
+    global gameArea, gameBoard, mapSize, surface, start_time, timeOver
     mapSize = 5
     cellSize = 32
     xPos = 289
     yPos = 231
     frame_count = 0
     frame_rate = 60
+    start_time = 0
     numTurns = 0
     play_again = 1
     proba=(0.05,0.30,0.6)
+    clockMode = False
     savePushed = False
     loadPushed = False
+    timeOver = False
     gameFinished = False
     firstSelection = False
     clock = pygame.time.Clock()
@@ -56,11 +56,14 @@ def main():
     selectedCellEffect = pygame.image.load("images/selected.png")
 
     # Sounds loading
+    musicChanged = False
     pygame.mixer.music.load('sounds/music1.ogg')
     oneUp = pygame.mixer.Sound('sounds/smb_1-up.wav')
     noAdj = pygame.mixer.Sound('sounds/smb_fireworks.wav')
     fuseUp = pygame.mixer.Sound('sounds/smb_jump.wav')
     newGame = pygame.mixer.Sound('sounds/smb_powerup.wav')
+    gameOver = pygame.mixer.Sound('sounds/smb_mariodie.wav')
+    wonGame = pygame.mixer.Sound('sounds/smb_stage_clear.wav')
 
     # Images object
     cells = {
@@ -84,12 +87,14 @@ def main():
     Grey = (110, 128, 145)
 
     # Functions
+    # Display the new cell gris
     def drawGrid(gameArea):
         for col in range(mapSize):
             for row in range(mapSize):
                 # Prepare to display correct numeric image according gameArea cell value in the correct position
                 surface.blit(cells[gameArea[col][row]], (yPos + row*cellSize, xPos + col*cellSize, cellSize, cellSize))
 
+    # Set startup vars and buttons
     def startupDisplay():
         scoreTitle = font.render('CURRENT SCORE :', True, Black)
         numTurnsTitle = font.render('NUMTURN COUNT :', True, Black)
@@ -97,7 +102,10 @@ def main():
         surface.blit(numTurnsTitle, (650, 120, 50, 50))
 
         # Draw buttons
-        global fourButton, fiveButton, sixButton, loadButton, saveButton, mainQuitButton
+        global timeButton, fourButton, fiveButton, sixButton, loadButton, saveButton, mainQuitButton
+        timeButton = pygame.draw.rect(surface, Grey, (815,220,85,23))
+        timeText = font.render("Time Limit", True, White)
+        surface.blit(timeText, (816,217,100,20))
         fourButton = pygame.draw.rect(surface, Grey, (637,245,85,23))
         fourText = font.render("4x4 Grid", True, White)
         surface.blit(fourText, (642,243,100,20))
@@ -118,7 +126,8 @@ def main():
         surface.blit(mainQuitText, (817,268,100,20))
         consoleText = fontSmall.render("Load / Save filename in console!", True, Red)
         surface.blit(consoleText, (6402,302,100,20))
-        
+
+    # Display score and maxValue of the game area    
     def displayScore():
         maxScore = maxValue(mapSize, gameArea)
         gameScore = font.render(str(maxScore), True, Black)
@@ -128,6 +137,7 @@ def main():
         surface.fill(White, (820, 120, 50, 50))
         surface.blit(numTurnsScore, (820, 120, 50, 50))
 
+    # Occult screen and show win or lost game status
     def gameWonOrOver(msg):
         # Draw end game screen
         text = fontBig.render(msg, True, White)
@@ -149,8 +159,9 @@ def main():
     # Choose file name at saving/loading file
     def choose():
         file = input('Enter file name : ')
+        file = 'save/' + file
         if len(file)<3 :
-            file = "save.txt"
+            file = "save/save.txt"
         else:
             try:
                 i = file.index('.')
@@ -181,18 +192,26 @@ def main():
         save.write(output)
         save.close()
 
+    # Load grid according to chosen file
     def loadGrid():
         file = choose()
-        gameArea = []
-        i = 0
-        with open(file, 'r') as openfile:
-            for line in openfile:
-                gameArea.append([])
-                gameArea[i] = [int(n) for n in line.split(',')]
-                i += 1
-        openfile.close()
+        try:
+            gameArea = []
+            i = 0
+            with open(file, 'r') as openfile:
+                for line in openfile:
+                    gameArea.append([])
+                    gameArea[i] = [int(n) for n in line.split(',')]
+                    i += 1
+            openfile.close()
+            return gameArea
+        except:
+            print("The file does not exists!")
+            loadGrid()
 
-        return gameArea
+    # When timer is near to be over music changing
+    def changeMusic():
+        pygame.mixer.music.load('sounds/music2.ogg')
     
     # Startup draw
     drawGrid(gameArea)
@@ -202,32 +221,64 @@ def main():
 
     # Main Loop
     while play_again:
-        total_seconds = frame_count // frame_rate
-        minutes = total_seconds // 60
-        seconds = total_seconds % 60
-        surface.fill(White, (740, 183, 60, 20))
-        output_string = "{0:02}:{1:02}".format(minutes, seconds)
-        timer = font.render(output_string, True, Black)
-        surface.blit(timer, [740, 180])
-        frame_count += 1
-        clock.tick(frame_rate)
-        
         # Calculate mousePos based on gameArea grid
         mouseX = (pygame.mouse.get_pos()[1] - xPos) // cellSize
         mouseY = (pygame.mouse.get_pos()[0] - yPos) // cellSize
 
         # Game Over
-        if not stillMoves(mapSize, gameArea) and gameFinished == False:
+        if not stillMoves(mapSize, gameArea) and gameFinished == False or timeOver == True :
+            pygame.mixer.music.stop()
+            gameOver.play()
             gameFinished = True
+            timeOver = False
             gameWonOrOver('Game Over !')
 
         # Game Won
         if maxValue(mapSize, gameArea) == 10 and gameFinished == False:
+            pygame.mixer.music.stop()
+            wonGame.play()
             gameFinished = True
             gameWonOrOver('You Win !')
 
+        # Displaying score& timer since while game not finished
         if not gameFinished:
             displayScore()
+
+            # Normal timer mode
+            if clockMode == False:
+                # Display timer while game is on
+                total_seconds = frame_count // frame_rate
+                minutes = total_seconds // 60
+                seconds = total_seconds % 60
+                surface.fill(White, (740, 183, 60, 20))
+                output_string = "{0:02}:{1:02}".format(minutes, seconds)
+                timer = font.render(output_string, True, Black)
+                surface.blit(timer, [740, 180])
+            else:
+                # Display countdown on clock game mode
+                total_seconds = start_time - (frame_count // frame_rate)
+                if total_seconds < 0:
+                    total_seconds = 0
+                minutes = total_seconds // 60
+                seconds = total_seconds % 60
+                surface.fill(White, (740, 183, 60, 20))
+                output_string = "{0:02}:{1:02}".format(minutes, seconds)
+                if minutes < 1: # Time almost done
+                    if musicChanged == False:
+                        pygame.mixer.music.stop()
+                        musicChanged = True
+                        changeMusic()
+                        pygame.mixer.music.play(-1)
+                    text = font.render(output_string, True, Red)
+
+                    if minutes == 0 and seconds == 0: timeOver = True # Game over
+                else:
+                    text = font.render(output_string, True, Black)
+                
+                surface.blit(text, [740, 180])
+
+            frame_count += 1
+            clock.tick(frame_rate)
 
         # Events loop
         for event in pygame.event.get():
@@ -248,8 +299,34 @@ def main():
                     pygame.display.set_caption("Super Mario Get Ten")
 
             # Buttons actions
+            elif event.type == MOUSEBUTTONDOWN and gameFinished:
+                # Game finished quit button
+                if quitButton.collidepoint(pygame.mouse.get_pos()):
+                    print('See u soon !')
+                    pygame.quit()
+                    sys.exit()
+
+                # Reload whole game (main function)
+                elif retryButton.collidepoint(pygame.mouse.get_pos()):
+                    main()
+
             elif event.type == MOUSEBUTTONDOWN:
+                # Time limit mode
+                if timeButton.collidepoint(pygame.mouse.get_pos()):
+                    clockMode = True
+                    frame_count = 0
+                    gameArea = gameBoard(mapSize, proba)
+                    drawGrid(gameArea)
+                    if mapSize == 4:
+                        start_time = 300
+                    elif mapSize == 5:
+                        start_time = 480
+                    elif mapSize == 6:
+                        start_time = 600
+
+                # 4x4 Grid
                 if fourButton.collidepoint(pygame.mouse.get_pos()):
+                    clockMode = False
                     frame_count = 0
                     mapSize = 4
                     gameArea = gameBoard(mapSize, proba)
@@ -257,7 +334,10 @@ def main():
                     drawGrid(gameArea)
                     startupDisplay()
                     newGame.play()
+
+                # 5x5 Grid
                 elif fiveButton.collidepoint(pygame.mouse.get_pos()):
+                    clockMode = False
                     frame_count = 0
                     mapSize = 5
                     gameArea = gameBoard(mapSize, proba)
@@ -265,7 +345,10 @@ def main():
                     drawGrid(gameArea)
                     startupDisplay()
                     newGame.play()
+
+                # 6x6 Grid
                 elif sixButton.collidepoint(pygame.mouse.get_pos()):
+                    clockMode = False
                     frame_count = 0
                     mapSize = 6
                     gameArea = gameBoard(mapSize, proba)
@@ -273,11 +356,16 @@ def main():
                     drawGrid(gameArea)
                     startupDisplay()
                     newGame.play()
+
+                # Same game to file
                 elif saveButton.collidepoint(pygame.mouse.get_pos()):
                     saveGrid()
+
+                # Load game from file
                 elif loadButton.collidepoint(pygame.mouse.get_pos()):
                     gameArea = loadGrid()
                     frame_count = 0
+                    clockMode = False
                     if len(gameArea) == 4:
                         mapSize = 4
                         surface = pygame.image.load("images/surface-4x4.png")
@@ -296,19 +384,14 @@ def main():
                         drawGrid(gameArea)
                         startupDisplay()
                         newGame.play()
+
+                # Main quit button
                 elif mainQuitButton.collidepoint(pygame.mouse.get_pos()):
                     print('See u soon !')
                     pygame.quit()
                     sys.exit()
-            
-            elif event.type == MOUSEBUTTONDOWN and gameFinished:
-                if quitButton.collidepoint(pygame.mouse.get_pos()):
-                    print('See u soon !')
-                    pygame.quit()
-                    sys.exit()
-                elif retryButton.collidepoint(pygame.mouse.get_pos()):
-                    main()
-            
+
+            # First selection on grid
             if firstSelection == False:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if mouseX >= 0 and mouseY >= 0:
@@ -323,7 +406,9 @@ def main():
                                     surface.blit(selectedCellEffect, (yPos + currentPos[1]*cellSize, xPos + currentPos[0]*cellSize, cellSize, cellSize))
                             else:
                                 # play error sound, no adjacent cells
-                                noAdj.play()  
+                                noAdj.play()
+            
+            # Second selection ...
             elif firstSelection == True:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if mouseX >= 0 and mouseY >= 0:
